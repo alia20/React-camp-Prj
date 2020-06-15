88c712e87b13708bdcd7677c2b9d95ff7d3e5b8c
alia20 <moh002335@gmail.com>
2020-06-15 12:54:44 -0500

    changed directory and capsiteinfo components into a functional components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class  CampsiteInfo  extends Component{
+
   
-    renderCampsite(campsite){
+  function  RenderCampsite({campsite}){
        return (
             <div className="col-md-5 m-1" > 
                 <Card>
@@ -16,7 +16,7 @@ class  CampsiteInfo  extends Component{
              </div> 
              );
     }
-    renderComments(comments) {
+  function  RenderComments({comments}) {
         if (comments){
             return(
                <div className="col-md-5 m-1">
@@ -27,13 +27,13 @@ class  CampsiteInfo  extends Component{
                   </div>)
         }
     }
-    render(){
-        if(this.props.campsite){
+ function CampsiteInfo(props){
+        if(props.campsite){
             return(
             <div className="container">
                <div className="row">
-                   {this.renderCampsite(this.props.campsite)}
-                   {this.renderComments(this.props.campsite.comments)}
+                    <RenderCampsite campsite={props.campsite} />
+                    <RenderComments comments={props.campsite.comments} />
                </div>
             </div>
             )
@@ -41,7 +41,7 @@ class  CampsiteInfo  extends Component{
        
            return <div/>;
         }
-}
+
 export default CampsiteInfo;
 
 
