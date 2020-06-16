c907ad95e0ad983eae6abc994f746be1dde58ab0
alia20 <moh002335@gmail.com>
2020-06-16 16:24:16 -0500

    added react router and home

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className="container">
+            <h4> my Home</h4>
+        </div>
+    );
+}
+
+export default Home;   
